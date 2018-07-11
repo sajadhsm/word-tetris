50f8dfc9016a2a9b9e43f59cb458a32edce563ee
Sajad Hashemian <sajadhsm@gmail.com>
2018-07-11 17:19:21 +0430

    Add keyboard movement support

--- src/components/Game/GameControls.vue
+++ src/components/Game/GameControls.vue
@@ -11,6 +11,20 @@ import { mapActions } from 'vuex';
 
 export default {
   name: 'GameControls',
+  // I think there is a better time to add keyDown listeners
+  // IMPORTANT: Dont forget to remove listeners then switching to
+  // other components
+  mounted() {
+    window.addEventListener('keydown', (e) => {
+      if (e.keyCode === 40) {
+        this.moveDown();
+      } else if (e.keyCode === 39) {
+        this.moveRight();
+      } else if (e.keyCode === 37) {
+        this.moveLeft();
+      }
+    });
+  },
   methods: mapActions([
     'moveLeft',
     'moveRight',
