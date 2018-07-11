c7e74c9267edf43c9c794ab35ff8a8acf9257e15
Sajad Hashemian <sajadhsm@gmail.com>
2018-07-12 02:03:58 +0430

    Add MainMenu Page

---
+++ src/components/Page/MainMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="main-menu">
+    <h1>واژه ماژه</h1>
+
+    <router-link
+      tag="button"
+      to="/game">شروع</router-link>
+
+    <button>جدول امتیازات</button>
+
+    <button>تنظیمات</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainMenu',
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2.8rem;
+}
+.main-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.main-menu button {
+  min-width: 200px;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  background: #fff;
+  border: 5px solid #ddd;
+  border-radius: 20px;
+  font-weight: 700;
+  font-size: 1.2rem;
+  cursor: pointer;
+  outline: none;
+}
+.main-menu button:last-child {
+  margin: 0;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import MainMenu from '@/components/Page/MainMenu.vue';
 import GameView from './views/Game.vue';
 
 Vue.use(Router);
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'main-menu',
+      component: MainMenu,
+    },
+    {
+      path: '/game',
       name: 'game-panel',
       component: GameView,
     },
