72993bd288e632c96e5a901c3c3c5a3fb4c2495f
Sajad Hashemian <sajadhsm@gmail.com>
2018-07-13 18:24:01 +0430

    Decreased GameHeaderStates spacing

--- src/components/Game/GameHeader.vue
+++ src/components/Game/GameHeader.vue
@@ -189,9 +189,19 @@ export default {
   background: rgba(5, 104, 170, 0.041);
 }
 .game-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   flex-basis: calc(100% / 3);
   padding: 0 5px;
   /* background: rgba(255, 47, 220, 0.137); */
+}
+/* Decrease GameHeaderStates spacing */
+.game-info > p {
+  margin: 0;
+}
+.game-info > p:not(:last-child) {
+  margin-bottom: 0.5rem;
 }
 .next-block-container {
   display: flex;
