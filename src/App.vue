7b770ca7b9aa973ab8c5e67391956ab8707b9e61
Sajad Hashemian <sajadhsm@gmail.com>
2018-07-18 21:36:42 +0430

    Move base.css and fonts to /src

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Game Panel</router-link> |
-      <a href="#!">Settings?</a>
-    </div> -->
-
     <div class="game-container">
       <router-view/>
     </div>
@@ -12,10 +7,63 @@
 </template>
 
 <style>
-#app {
+@font-face {
+  font-family: Shabnam;
+  src: url('assets/fonts/Shabnam.eot');
+  src: url('assets/fonts/Shabnam.eot?#iefix') format('embedded-opentype'),
+       url('assets/fonts/Shabnam.woff') format('woff'),
+       url('assets/fonts/Shabnam.ttf') format('truetype');
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: Shabnam;
+  src: url('assets/fonts/Shabnam-Bold.eot');
+  src: url('assets/fonts/Shabnam-Bold.eot?#iefix') format('embedded-opentype'),
+       url('assets/fonts/Shabnam-Bold.woff') format('woff'),
+       url('assets/fonts/Shabnam-Bold.ttf') format('truetype');
+  font-weight: bold;
+}
+
+html {
+  height: 100%;
+}
+
+body {
   font-family: 'Shabnam', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  background-color: #f9f9f9;
+}
+
+.col {
+  width: 2.5rem;
+  height: 2.5rem;
+  margin-right: 0.5rem;
+  border-radius: 1.05rem;
+  background: #ddd;
+  color: #fff;
+  font-size: 1.1rem;
+  font-weight: 700;
+  overflow: hidden;
+}
+.col:last-child {
+  margin: 0;
+}
+
+.block {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.35);
+}
+
+@media screen and (min-width: 768px) {
+  html {
+    font-size: 24px;
+  }
 }
 </style>
