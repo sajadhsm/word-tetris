78fb9584d43cd18f3c61717411e69c406998c227
Sajad Hashemian <sajadhsm@gmail.com>
2018-07-20 13:35:51 +0430

    Update levels

--- src/data/levels.js
+++ src/data/levels.js
@@ -2,9 +2,440 @@ export default [
   {
     word: 'بهار',
     type: 'idiom',
+    speed: 2000,
     hint: {
       prefix: 'با یک گل',
-      sufix: 'نمیشه!',
+      sufix: 'نمیشه',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'برکت',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'از تو حرکت از خدا',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'فرجه',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'از این ستون به ان ستون',
+    },
+    time: {
+      seconds: 50,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'وخیم',
+    type: 'synonym',
+    speed: 2000,
+    hint: 'مخاطره آمیز',
+    time: {
+      seconds: 20,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'خشت',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'یک',
+      sufix: 'هم بگذار در دیگ',
+    },
+    time: {
+      seconds: 20,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'سوخته',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'آش نخورده و دهن',
+
+    },
+    time: {
+      seconds: 45,
+      minutes: 2,
+    },
+  },
+  {
+    word: 'صلب',
+    type: 'synonym',
+    hint: 'جامد',
+    speed: 2000,
+    time: {
+      seconds: 45,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'هاون',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'اب در',
+      sufix: 'کوبیدن',
+    },
+    time: {
+      seconds: 40,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'افتاب',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      sufix: 'عمر لب لومه',
+    },
+    time: {
+      seconds: 45,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'آسمان',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      sufix: 'همه جاش یه رنگه',
+    },
+    time: {
+      seconds: 45,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'هندوانه',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'با یک دست دو',
+      sufix: 'نمیتوان برداشت',
+    },
+    time: {
+      seconds: 45,
+      minutes: 2,
+    },
+  },
+  {
+    word: 'دریا',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'آب از',
+      sufix: 'بخشیدن',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'چشم',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'پشت',
+      sufix: 'نازک کردن',
+    },
+    time: {
+      seconds: 20,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'گربه',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'به دعای',
+      sufix: 'سیاه بارون نمیاد',
+    },
+    time: {
+      seconds: 45,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'سرنگون',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'فواره چون بلند شود',
+      sufix: 'شود',
+    },
+    time: {
+      seconds: 30,
+      minutes: 2,
+    },
+  },
+  {
+    word: 'رونده',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'از اینحا مونده از اونجا',
+    },
+    time: {
+      seconds: 30,
+      minutes: 2,
+    },
+  },
+  {
+    word: 'پیچیده',
+    type: 'synonym',
+    hint: 'غامض',
+    speed: 2000,
+    time: {
+      seconds: 35,
+      minutes: 2,
+    },
+  },
+  {
+    word: 'نشون',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'با یه تیر دو',
+      sufix: 'زدن',
+    },
+    time: {
+      seconds: 45,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'کوزه',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'خیاط در',
+      sufix: 'افتاد',
+    },
+    time: {
+      seconds: 10,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'مرگ',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'توبه ی گرگ',
+      sufix: 'است',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'بضاعت',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'تواتنگری به قناعت به است توانگری به',
+    },
+    time: {
+      seconds: 9,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'تاریکی',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'تیر توی',
+      sufix: 'انداختن',
+    },
+    time: {
+      seconds: 35,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'مردگی',
+    type: 'idiom',
+    speed: 2000,
+    hint: {
+      prefix: 'خود را به موش',
+      sufix: 'زدن',
+    },
+    time: {
+      seconds: 35,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'باقالی',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'شتر بیار و',
+      sufix: 'بار کن',
+    },
+    time: {
+      seconds: 30,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'شنیدن',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'کی بود مانند دیدن',
+    },
+    time: {
+      seconds: 27,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'زیادی',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'سرش به تنش',
+      sufix: 'کرده',
+    },
+    time: {
+      seconds: 15,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'شاهنامه',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'اخرش خوش است',
+    },
+    time: {
+      seconds: 0,
+      minutes: 2,
+    },
+  },
+  {
+    word: 'سفید',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'پایان شب سیه',
+      sufix: 'است',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'ضامن',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'روزی بود روزی رسان',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'طاقتم',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'طاق',
+      sufix: 'شد',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'اشارت',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'عاقل را یک',
+      sufix: 'بس بود',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'حقدار',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'عاقبت حق به',
+      sufix: 'رسید',
+    },
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'فراغت',
+    type: 'synonym',
+    hint: 'اسودگی',
+    speed: 1000,
+    time: {
+      seconds: 2,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'حارس',
+    type: 'synonym',
+    hint: 'نگهبان',
+    speed: 1000,
+    time: {
+      seconds: 55,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'جوینده',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'عاقبت',
+      sufix: 'یابنده بود',
+    },
+    time: {
+      seconds: 10,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'هشتش',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      sufix: 'گرو نهش است',
     },
     time: {
       seconds: 45,
@@ -12,28 +443,169 @@ export default [
     },
   },
   {
-    word: 'آسمان',
+    word: 'مختوم',
+    type: 'idiom',
+    speed: 1000,
+    hint: 'پایان',
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'مفروز',
+    type: 'idiom',
+    hint: 'جدا کردن',
+    speed: 1000,
+    time: {
+      seconds: 0,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'پیشرفت',
     type: 'opposite',
-    hint: 'زمین',
+    hint: 'پس رفت',
+    speed: 1000,
+    time: {
+      seconds: 3,
+      minutes: 1,
+    },
+  },
+  {
+    word: 'مستعجل',
+    type: 'idiom',
+    speed: 1000,
+    hint: {
+      prefix: 'کارها به صبر براید',
+      sufix: 'به سر دراید',
+    },
     time: {
       seconds: 30,
       minutes: 1,
     },
   },
   {
-    word: 'هندوانه',
+    word: 'ذلیل',
+    type: 'opposite',
+    hint: 'خواری',
+    speed: 1000,
     time: {
-      seconds: 10,
-      minutes: 1,
+      seconds: 40,
+      minutes: 0,
     },
   },
   {
-    word: 'صلب',
+    word: 'دوری',
     type: 'synonym',
-    hint: 'جامد',
+    hint: 'فرقت',
+    speed: 900,
+    time: {
+      seconds: 40,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'بزرگی',
+    type: 'synomym',
+    hint: 'عزیز',
+    speed: 900,
+    time: {
+      seconds: 50,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'سیاهی',
+    type: 'idiom',
+    speed: 800,
+    hint: {
+      prefix: 'بالاتر از',
+      sufix: 'رنگی نیست',
+    },
+    time: {
+      seconds: 45,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'حلوا',
+    type: 'idiom',
+    speed: 900,
+    hint: {
+      prefix: 'تو دعوا',
+      sufix: 'خیرات نمیکنن',
+    },
+    time: {
+      seconds: 35,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'غوره',
+    type: 'idiom',
+    speed: 900,
+    hint: {
+      prefix: 'گر صبر کنی ز',
+      sufix: 'حلوا سازی',
+    },
     time: {
       seconds: 30,
-      minutes: 1,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'قسمت',
+    type: 'idiom',
+    speed: 900,
+    hint: {
+      sufix: 'را باور کنم یا دم خروس را',
+    },
+    time: {
+      seconds: 25,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'پیشانی',
+    type: 'idiom',
+    speed: 900,
+    hint: {
+      prefix: 'گاو',
+      sufix: 'سفید',
+    },
+    time: {
+      seconds: 20,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'کهنسال',
+    type: 'synomym',
+    speed: 900,
+    hint: 'پیر',
+    time: {
+      seconds: 45,
+      minutes: 0,
+    },
+  }, {
+    word: 'سوغات',
+    type: 'synomym',
+    speed: 900,
+    hint: 'تحفه',
+    time: {
+      seconds: 30,
+      minutes: 0,
+    },
+  },
+  {
+    word: 'صلح',
+    type: 'opposite',
+    hint: 'جنگ',
+    speed: 900,
+    time: {
+      seconds: 20,
+      minutes: 0,
     },
   },
 ];
